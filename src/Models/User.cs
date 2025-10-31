8bfb4fd8eac1924164d70cf9e6f284df26d7ab83
Jairo Calcina <j.calcina.v@gmail.com>
2025-10-31 16:09:01 -0300

    feat: add user model

---
+++ src/Models/User.cs
@@ -0,0 +1,10 @@
+namespace CensudexOrders.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string LastNames { get; set; } = default!;
+    public string Address { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
