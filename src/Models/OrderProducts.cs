4309fd4482f0911a473e168422fbb38571e96819
Jairo Calcina <j.calcina.v@gmail.com>
2025-10-31 16:10:06 -0300

    feat: add order products model

---
+++ src/Models/OrderProducts.cs
@@ -0,0 +1,9 @@
+namespace CensudexOrders.Models;
+
+public class OrderProducts
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
