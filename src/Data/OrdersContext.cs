5347aff540abd160a1d26c88a5e3072b07056e94
Jairo Calcina <j.calcina.v@gmail.com>
2025-11-09 20:23:32 -0300

    feat: add remaining tables to database context

--- src/Data/OrdersContext.cs
+++ src/Data/OrdersContext.cs
@@ -6,7 +6,20 @@ namespace CensudexOrders.Data;
 public class OrdersContext : DbContext
 {
     public DbSet<Order> Orders { get; set; } = default!;
+    public DbSet<Product> Products { get; set; } = default!;
+    public DbSet<OrderProducts> OrderProducts { get; set; } = default!;
+    public DbSet<User> Users { get; set; } = default!;
     public OrdersContext(DbContextOptions<OrdersContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>()
+            .Property(o => o.OrderNumber)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Order>()
+            .HasAlternateKey(o => o.OrderNumber);
     }
 }
