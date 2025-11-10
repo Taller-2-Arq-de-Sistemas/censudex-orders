12cef0fd6ffa1c24164f5c7d2b28ce88dc9948ad
Jairo Calcina <j.calcina.v@gmail.com>
2025-11-09 22:02:37 -0300

    feat: add app setup

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using CensudexOrders.Behaviors;
+using CensudexOrders.Data;
 using CensudexOrders.Services;
+using CensudexOrders.Repositories.Interfaces;
+using CensudexOrders.Repositories;
+using CensudexOrders.Extensions;
+using CensudexOrders.Exceptions;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddDbContext<OrdersContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("Database") ?? throw new InvalidOperationException("Connection string 'Database' not found."),
+        new MySqlServerVersion(new Version(9, 4, 0))
+    ));
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc(options =>
+{
+    options.Interceptors.Add<GlobalExceptionHandler>();
+});
+builder.Services.AddSingleton<GlobalExceptionHandler>();
+builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IOrderProductsRepository, OrderProductsRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<OrdersService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+AppSeedService.SeedDatabase(app);
 
 app.Run();
