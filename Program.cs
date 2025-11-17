58f96be636c75fce9a27744e1254dcc2f017901c
Jairo Calcina <j.calcina.v@gmail.com>
2025-11-16 22:34:09 -0300

    feat: add app setup

--- Program.cs
+++ Program.cs
@@ -6,16 +6,50 @@ using CensudexOrders.Repositories.Interfaces;
 using CensudexOrders.Repositories;
 using CensudexOrders.Extensions;
 using CensudexOrders.Exceptions;
+using CensudexOrders.MessageBroker.Configuration;
+using CensudexOrders.MessageBroker.Interfaces;
+using CensudexOrders.MessageBroker.Publishers;
+using CensudexOrders.MessageBroker.Workers;
 using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+// Configure RabbitMQ settings
+builder.Services.Configure<RabbitMqSettings>(
+    builder.Configuration.GetSection(RabbitMqSettings.SectionName));
+
+// Register RabbitMQ services
+builder.Services.AddSingleton<IRabbitMqConnection, RabbitMqConnection>();
+builder.Services.AddSingleton<IEventPublisher, RabbitMqEventPublisher>();
+
+// Register outbox repository
+builder.Services.AddScoped<IOutboxRepository, OutboxRepository>();
+
+// Register processed events repository
+builder.Services.AddScoped<IProcessedEventsRepository, ProcessedEventsRepository>();
+
+// Register event consumers
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.UserCreatedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.UserUpdatedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.UserDeletedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.ProductCreatedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.ProductUpdatedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.ProductDeletedConsumer>();
+builder.Services.AddScoped<CensudexOrders.MessageBroker.Consumers.OrderCancelledByInsufficientStockConsumer>();
+
+// Register background workers
+builder.Services.AddHostedService<OutboxProcessorWorker>();
+builder.Services.AddHostedService<RabbitMqConsumerWorker>();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(CommandValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(QueryValidationBehavior<,>));
+    // NOTE: DomainEventDispatcherBehavior removed - domain events now dispatched in OrdersContext.SaveChangesAsync()
 });
 builder.Services.AddDbContext<OrdersContext>(options =>
     options.UseMySql(
@@ -33,6 +67,8 @@ builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
 builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 builder.Services.AddScoped<IOrderProductsRepository, OrderProductsRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<CensudexOrders.Services.Interfaces.IEmailTemplateService, CensudexOrders.Services.EmailTemplateService>();
+builder.Services.AddScoped<CensudexOrders.Services.Interfaces.ISendGridService, SendGridService>();
 
 var app = builder.Build();
 
