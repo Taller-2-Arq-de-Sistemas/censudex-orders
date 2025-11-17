67f1ad61818a2e9abbd3c4836f76fe76678eaba6
Jairo Calcina <j.calcina.v@gmail.com>
2025-11-16 22:26:42 -0300

    feat: change price data type

--- src/Models/OrderProducts.cs
+++ src/Models/OrderProducts.cs
@@ -8,5 +8,5 @@ public class OrderProducts
     public Guid ProductId { get; set; }
     public Product Product { get; set; } = default!;
     public int Quantity { get; set; }
-    public decimal Price { get; set; }
+    public int Price { get; set; }
 }
